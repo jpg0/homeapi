a5a4133155b7237825060bc784e52055ea89e067
Jonathan Gilbert <jgilbert@atlassian.com>
2016-12-27 14:20:26 +1100

    now uses sonarr client

--- potential_downloads.go
+++ potential_downloads.go
@@ -3,6 +3,8 @@ package main
 import (
 	"strings"
 	"github.com/Sirupsen/logrus"
+	"github.com/jpg0/go-sonarr"
+	"github.com/juju/errors"
 )
 
 func InitDownloadActions() {
@@ -25,10 +27,19 @@ func PotentialDownloads(ac *ActionContext, cfg map[string]string) (*ActionRespon
 		switch strings.ToLower(showtype) {
 		case "tv":
 			ac.Add("showtype", "tv")
-			AddPotentialTVDownloads(showname, ac)
+			err := AddPotentialTVDownloads(showname, ac, cfg)
+
+			if err != nil {
+				return nil, errors.Annotate(err, "Failed to lookup TV shows")
+			}
+
 		case "movie":
 			ac.Add("showtype", "movie")
-			AddPotentialMovieDownloads(showname, ac)
+			err := AddPotentialMovieDownloads(showname, ac, cfg)
+
+			if err != nil {
+				return nil, errors.Annotate(err, "Failed to lookup movies")
+			}
 		default:
 			logrus.Infof("Unknown show type %v", showtype)
 			ac.Remove("showtype")
@@ -41,10 +52,38 @@ func PotentialDownloads(ac *ActionContext, cfg map[string]string) (*ActionRespon
 	return ac.WriteTo(&ActionResponse{}), nil
 }
 
-func AddPotentialTVDownloads(showname string, ac *ActionContext) {
-	ac.Add("singleshowoption", "my movie!")
+func AddPotentialTVDownloads(showname string, ac *ActionContext, cfg map[string]string) error {
+	sc, err := go_sonarr.NewSonarrClient(cfg["sonarr_address"], cfg["sonarr_apikey"])
+
+	if err != nil {
+		return errors.Annotate(err, "Failed to create Sonarr client")
+	}
+
+	slr, err := sc.SeriesLookup(showname)
+
+	if err != nil {
+		return errors.Annotate(err, "Failed to call Sonarr")
+	}
+
+	if len(slr) == 1 {
+		ac.Add("singleshowoption", slr[0].Title)
+	} else {
+
+		shows := ""
+
+		for _, show := range slr {
+			shows += show.Title + "\n"
+		}
+
+
+		ac.Add("multipleshowoption", shows)
+	}
+
+	return nil
 }
 
-func AddPotentialMovieDownloads(showname string, ac *ActionContext) {
+func AddPotentialMovieDownloads(showname string, ac *ActionContext, cfg map[string]string) error {
 	ac.Add("singleshowoption", "my tv show!")
+
+	return nil
 }
