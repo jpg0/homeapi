5483301a015a28b29e8a95883e51a7fda109cd83
Jonathan Gilbert <jgilbert@atlassian.com>
2016-12-31 12:59:41 +1100

    added default

--- photos_handler.go
+++ photos_handler.go
@@ -6,16 +6,24 @@ import (
 	"fmt"
 )
 
+const DEFAULT_PHOTOS_PATH = "/photos/*.*"
+
 func InitPhotosActions() {
 	Register("photo_uploads", PhotoUploads)
 }
 
 func PhotoUploads(ac *ActionContext, cfg map[string]string) (*ActionResponse, error) {
 
-	files, err := filepath.Glob(cfg["photos_path"])
+	var path string
+	var present bool
+	if path, present = cfg["photos_path"] ; !present {
+		path = DEFAULT_PHOTOS_PATH
+	}
+
+	files, err := filepath.Glob(path)
 
 	if err != nil {
-		return nil, errors.Annotatef(err, "Failed to list files for %v", "photos_path")
+		return nil, errors.Annotatef(err, "Failed to list files for %v", path)
 	}
 
 	ac.Add("awaiting_upload", fmt.Sprintf("%v", len(files)))
